0a5b184e6b46c9b0ab403d86f93c3e6101b2f4e3
Francois Groleau <francois.groleau@nuance.com>
2025-02-17 15:01:36 -0500

    add userInfo to home

--- app/(features)/home/page.tsx
+++ app/(features)/home/page.tsx
@@ -1,12 +1,24 @@
 import styles from "./page.module.css";
 
-export default function LandingPage() {
+async function getUserInfo() {
+  const response = await fetch('/.auth/me');
+  const payload = await response.json();
+  const { clientPrincipal } = payload;
+  return clientPrincipal;
+}
+
+export default async function LandingPage() {
+  const userInfo = await getUserInfo();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>
           Home
         </h1>
+        <p>
+          {JSON.stringify(userInfo)}
+        </p>
       </div>
     </main>
   );
