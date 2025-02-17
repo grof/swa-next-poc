d33ffea559fcaf551f7176aebcbe81831ab3d216
Francois Groleau <francois.groleau@nuance.com>
2025-02-17 15:54:52 -0500

    update library to get user info server side

--- app/(features)/library/page.tsx
+++ app/(features)/library/page.tsx
@@ -1,12 +1,42 @@
+import { headers } from 'next/headers';
 import styles from "./page.module.css";
 
-export default function LibraryPage() {
+interface User {
+  identityProvider: string;
+  userId: string;
+  userDetails: string;
+  userRoles: string[];
+}
+
+async function getUser(): Promise<User | null> {
+  const headersList = headers();
+  const user = headersList.get('x-ms-client-principal');
+
+  if (user) {
+    const decodedUser = JSON.parse(Buffer.from(user, 'base64').toString('utf-8'));
+    return decodedUser;
+  }
+
+  return null;
+}
+
+export default async function LibraryPage() {
+  const user = await getUser();
+
+  if (!user) {
+    return <div>Please log in to view your profile</div>
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>
           Library
         </h1>
+        <p><strong>Provider:</strong> {user.identityProvider}</p>
+        <p><strong>User ID:</strong> {user.userId}</p>
+        <p><strong>User Details:</strong> {user.userDetails}</p>
+        <p><strong>Roles:</strong> {user.userRoles.join(', ')}</p>
       </div>
     </main>
   );
